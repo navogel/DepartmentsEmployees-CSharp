e31f1481e399e7d1f569106940a7551d837ee700
Nate V <navogel@gmail.com>
2019-12-09 11:25:34 -0600

    first commit

---
+++ DepartmentsEmployees/Program.cs
@@ -0,0 +1,33 @@
+﻿using DepartmentsEmployees.Data;
+using DepartmentsEmployees.Models;
+using System;
+using System.Collections.Generic;
+
+namespace DepartmentsEmployees
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var departmentRepo = new DepartmentRepository();
+
+            
+
+            Console.WriteLine("Getting All Departments:");
+            Console.WriteLine();
+
+            List<Department> allDepartments = departmentRepo.GetAllDepartments();
+
+            foreach (Department dept in allDepartments)
+            {
+                Console.WriteLine($"{dept.Id} {dept.DepartmentName}");
+            }
+
+            var hardCodedId = 3;
+            var departmentWithId3 = departmentRepo.GetDepartmentById(hardCodedId);
+            Console.WriteLine(".............................");
+            Console.WriteLine($"Department with Id {hardCodedId} is {departmentWithId3.DepartmentName}");
+
+        }
+    }
+}
